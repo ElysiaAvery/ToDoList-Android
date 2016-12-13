a3361a9bd1add22ef1945a64b32af20b1b73f943
Elysia Nason and Tim Jung <elysia+tim@>
2016-12-12 16:08:01 -0800

    Turn Ewa's phone into a shake weight

--- app/src/main/java/com/example/guest/todolist/MainActivity.java
+++ app/src/main/java/com/example/guest/todolist/MainActivity.java
@@ -1,21 +1,60 @@
 package com.example.guest.todolist;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.listView) ListView mListView;
-
+    private String[] tasks = new String[] {"Wash Dog", "Vacuum Living Room", "Make Bed", "Cook Food For Family", "Plan Lunches", "Go Grocery Shopping", "Call Mom"};
+    @Bind(R.id.header) TextView mHeader;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeListener mShakeDetector;
+    int mCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks);
+        mListView.setAdapter(adapter);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeListener();
+        mShakeDetector.setOnShakeListener(new ShakeListener.OnShakeListener() {
+            @Override
+            public void onShake(int count) {
+//                count += 1;
+                mCount += count;
+                mHeader.setText("Shake Count" + mCount);
+            }
+        });
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
 }
